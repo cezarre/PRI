938762dd9767b1c1091801c2925203cfa05e012a
cezarre <cez@c68-251.wmi.amu.edu.pl>
2015-03-30 17:30:03 +0200

    poprawka

---
+++ Scripts/Point.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Point : MonoBehaviour {
+
+		private int x;
+		private int y;
+
+	public void Set(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int GetX(){
+		return x;
+	}
+
+	public int GetY() {
+		return y;
+	}
+
+}
---
+++ Scripts/Towers.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+public class Towers : MonoBehaviour {
+
+	List<int> list = new List<int>();
+	//----------------------------------
+	public int T_level;
+	public int demage;
+	public int perSec;
+	public int radius;
+	public Point TowerCord = new Point();
+
+	public Towers(int x, int y){
+		this.TowerCord.Set (x, y);
+	}
+
+	void ActionOnEnemy(){
+		//for all enemies
+
+	}
+	void Shoot() {
+
+		if (
+	}
+
+	void LevelUpgrade(){
+		//demage = 
+	}
+
+	int distance(Point a, Point b) {
+		//calculate distanc
+		int x = a.GetX () - b.GetX ();
+		int y = a.GetY () - b.GetY ();
+
+		return Mathf.Sqrt (x * x + y * y);
+	}
+
+	bool DetectedEnemy(Point EnemyCord) {
+		if (distance (EnemyCord, TowerCord) <= radius) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
