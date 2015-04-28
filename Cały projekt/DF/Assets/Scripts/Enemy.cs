f46066ec6bf0ef51de887f23ed85424aaad596fa
l96s1 <patrykbern@gmail.com>
2015-04-28 14:54:35 +0200

    Nowy folder CAŁEGO projektu
    Wklejam cały folder nowego projektu.

---
+++ Cały projekt/DF/Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+
+	public Point myPoint = new Point ();
+	private int hp;
+
+	public Enemy (int x, int y)
+	{
+		this.myPoint.Set (x, y);
+	}
+
+	public void damage (int d)
+	{
+		hp -= d;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
---
+++ Cały projekt/DF/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour {
+	public float speed; //predkosc wroga
+	Rigidbody2D player;
+	bool firstPath = true;
+	public float hp;
+
+	Animator anim;
+
+	void Start () {
+		//hp = 10;
+		anim = GetComponent<Animator> ();
+		player = GetComponent<Rigidbody2D> ();
+
+	}
+
+
+
+	public void damage (float d)
+	{
+		hp -= d;
+		if (hp <= 0) {
+			Destroy (gameObject);
+		}
+	}
+	public void EndPointReached()
+	{
+		Destroy (gameObject);
+	}
+	void FixedUpdate ()
+	{
+		ChangeDirection ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+	void OnTriggerEnter2D(Collider2D col)
+
+	{
+
+
+		//player = GetComponent<Rigidbody2D> ();
+		//Debug.Log("Something has entered this zone."+ col.gameObject.tag);
+
+		if (firstPath) {
+			firstPath = false;
+			//pierwsze wejscie na sciezke
+			if (col.gameObject.tag == "RTLTR" || col.gameObject.tag == "RTUTR" || col.gameObject.tag == "RTDTR") {
+			
+				player.AddForce (new Vector2 (-(speed), 0f));
+
+			
+			}else
+			if (col.gameObject.tag == "UTDTU" || col.gameObject.tag == "UTLTU" || col.gameObject.tag == "UTRTU" ) {
+				
+				player.AddForce (new Vector2 (0f, -speed));
+				
+			}else
+			if (col.gameObject.tag == "DTUTD" || col.gameObject.tag == "DTLTD" || col.gameObject.tag == "DTRTD") {
+				
+				player.AddForce (new Vector2 (0f, speed));
+				
+			}else
+			if (col.gameObject.tag == "LTDTL" || col.gameObject.tag == "LTUTL" || col.gameObject.tag == "LTRTL") {
+				
+				player.AddForce (new Vector2 (speed, 0f));
+				
+			}
+
+		} else {
+
+			//zakrety
+			if (col.gameObject.tag == "UTLTL" || col.gameObject.tag == "LTUTU") {
+				
+				player.AddForce (new Vector2 (-speed, speed));
+			} else
+
+			if (col.gameObject.tag == "UTRTR" || col.gameObject.tag == "RTUTU") {
+				
+				player.AddForce (new Vector2 (speed, speed));
+			}else
+			if (col.gameObject.tag == "DTLTL" || col.gameObject.tag == "LTDTD") {
+				
+				player.AddForce (new Vector2 (-speed, -speed));
+			}else
+			if (col.gameObject.tag == "DTRTR" || col.gameObject.tag == "RTDTD") {
+				
+				player.AddForce (new Vector2 (speed, -speed));
+			}
+
+		}
+	}
+	void ChangeDirection()
+	{
+
+		//player = GetComponent<Rigidbody2D> ();
+		if (player.velocity.y < 0) { //to down
+				anim.SetInteger ("Direction", 1);
+		} else 
+			if (player.velocity.y > 0) {//to up
+				anim.SetInteger ("Direction", 3);
+		} else
+			if (player.velocity.x < 0) { //to left
+				anim.SetInteger ("Direction", 0);
+		} else
+			if (player.velocity.x > 0) {//to right
+				anim.SetInteger ("Direction", 2);
+		}
+		Debug.Log (anim.GetInteger("Direction").ToString ());
+
+	}
+}
