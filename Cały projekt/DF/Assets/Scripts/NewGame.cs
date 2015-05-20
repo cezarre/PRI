1057da869b0e4e606af353d362ffbef6079588f9
l96s1 <patrykbern@gmail.com>
2015-05-20 23:11:04 +0200

    Roberta commit

---
+++ Cały projekt/DF/Assets/Scripts/NewGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewGame : MonoBehaviour {
+
+	// Use this for initialization
+	public void Start () {
+		Application.LoadLevel("exampleScene");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
