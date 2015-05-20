e9f317bc6d4602f804358f4850f1b47445152df1
l96s1 <patrykbern@gmail.com>
2015-05-20 23:29:57 +0200

    Roberta - dodanie menu

--- Cały projekt/DF/Assets/Scripts/NewGame.cs
+++ Cały projekt/DF/Assets/Scripts/NewGame.cs
@@ -5,8 +5,12 @@ public class NewGame : MonoBehaviour {
 
 	// Use this for initialization
 	public void Start () {
+
+	}
+	public void LoadScene(){
 		Application.LoadLevel("exampleScene");
 	}
+
 	
 	// Update is called once per frame
 	void Update () {
