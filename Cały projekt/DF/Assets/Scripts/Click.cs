fc954e74fa19c98e7ddb5187342e2be80bfd8776
cezarre <cezarre@gmail.com>
2016-01-12 00:29:02 +0100

    Wyłączenie TowerUI kliknięciem poza obszar UI - działa tylko przed budową wież.

---
+++ Cały projekt/DF/Assets/Scripts/Click.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Click : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        print("CLICK");
+        bool isClicked = transform.parent.gameObject.GetComponent<Towers>().isClicked;
+
+        transform.parent.transform.FindChild("Tower-UI").gameObject.SetActive(false);
+
+    }
+}
